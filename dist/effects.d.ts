e71eebb7911ae36229244d9c2b715a42bbd3e983
dgknca <dgknca@gmail.com>
2021-05-19 18:08:55 +0300

    feature: allow raw html

--- dist/effects.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const fadeIn: (self: any) => void;
-export declare const fadeOut: (self: any) => void;
-export declare const slideIn: (self: any) => void;
-export declare const slideOut: (self: any) => void;
