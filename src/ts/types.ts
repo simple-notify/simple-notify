4863fd6718dcd8f499ca38e3505733ff80cfa95b
alpercun <alper-aga97@hotmail.com>
2021-10-08 17:45:31 +0300

    Add auto close timeout progress animation to notifications

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,8 +1,24 @@
 type notifyType = 1 | 2 | 3
-type notifyStatus = 'success' | 'warning' | 'error'
+type notifyStatus = 'success' | 'warning' | 'error' | 'info'
 type notifyEffect = 'fade' | 'slide'
-type notifyPosition = 'left top' | 'top left' | 'right top' | 'top right' | 'left bottom' | 'bottom left' | 'right bottom' | 'bottom right' |
-  'center' | 'left y-center' | 'right y-center' | 'y-center left' | 'y-center right' | 'top x-center' | 'bottom x-center' | 'x-center top' | 'x-center bottom'
+type notifyPosition =
+  | 'left top'
+  | 'top left'
+  | 'right top'
+  | 'top right'
+  | 'left bottom'
+  | 'bottom left'
+  | 'right bottom'
+  | 'bottom right'
+  | 'center'
+  | 'left y-center'
+  | 'right y-center'
+  | 'y-center left'
+  | 'y-center right'
+  | 'top x-center'
+  | 'bottom x-center'
+  | 'x-center top'
+  | 'x-center bottom'
 
 interface IArgs {
   status?: notifyStatus
@@ -11,12 +27,12 @@ interface IArgs {
   position?: notifyPosition
   title?: string
   text?: string
-  showIcon?: Boolean
+  showIcon?: boolean
   customIcon?: string
-  showCloseButton?: Boolean
+  showCloseButton?: boolean
   customClass?: string
   speed?: number
-  autoclose?: Boolean
+  autoclose?: boolean
   autotimeout?: number
   gap?: number
   distance?: number
